c02aca469a7cc77eed3f5d7eabeaa68458a58983
shenal chanuka <125878126+shenalchanu8@users.noreply.github.com>
2024-03-16 16:28:08 +0530

    tra2

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -51,14 +51,7 @@ router.post('/get-user-info-by-id', authMiddleware, async (req,res) => {
         if (!user) {
             return res.status(200).send({ message: "User does not exist", success: false });
         } else {
-            res.status(200).send({ message: "User found", success: true, data: {
-
-                name: user.name,
-                email: user.email,
-
-            }
-                
-
+            res.status(200).send({ message: "User found", success: true, data: user
         });
         }
     } catch (error) {
