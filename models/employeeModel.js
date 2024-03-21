a577300d95bd026a3132af51416d6b3d3494e5a1
Devangap <101203575+Devangap@users.noreply.github.com>
2024-03-21 09:44:57 +0530

    Update employeeModel.js

--- models/employeeModel.js
+++ models/employeeModel.js
@@ -39,6 +39,25 @@ const userSchema = new mongoose.Schema({
     address: {
         type: String,
         required: true
+    },
+    isDoctor:{
+        type:Boolean,
+        default:false,
+
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false,
+
+    },
+    seenNotifications:{
+        type :Array,
+        default:[],
+    },
+    unseenNotifications:{
+        type :Array,
+        default:[],
+
     }
 }, {
     timestamps: true
