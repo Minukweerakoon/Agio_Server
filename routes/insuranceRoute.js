808683b69ffbcfafc4b570018687e4261bf971e5
DularaLiyanage <dularaliyanage1651@gmail.com>
2024-04-04 12:18:59 +0530

    File Upload done
    But name not showing in the database

--- routes/insuranceRoute.js
+++ routes/insuranceRoute.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const Insurance = require('../models/insuranceModel');
+const upload = require('../middleware/upload')
 
-router.post('/insClaimSubmit' , async(req, res) => {
-
+router.post('/insClaimSubmit', upload.single('file'), async (req, res) => {
     try {
-        const newInsurance = new Insurance(req.body); 
+        const { name, id, phoneNumber } = req.body;
+        const file = req.file.filename; // Get the filename of the uploaded file
+
+        const newInsurance = new Insurance({ name, id, phoneNumber, file }); // Include file in the Insurance model
         await newInsurance.save();
+        
         res.status(200).send({ message: "Claim submission successful.", success: true });
     } catch (error) {
         console.error(error);
         res.status(500).send({ message: "Claim submission unsuccessful.", success: false, error });
     }
-})
+});
 
+router,get('/')
+s
 module.exports = router;
