c768b6c45a7223c752c4044bee1ae719f31fb531
thinalfdo <thinalfernando1971@gmail.com>
2024-04-07 12:56:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/api/leave', leaveRoute);
 
 
 
-//const workoutRoutes= require('./routes/annWorkouts');//from routes
+const workoutRoutes= require('./routes/annWorkouts');//from routes
 
 
 
@@ -30,7 +30,7 @@ app.use((req,res,next)=>{
     console.log(req.path,req.method)
     next()
  }) 
- //app.use('/api/annWorkouts',workoutRoutes)
+ app.use('/api/annWorkouts',workoutRoutes)
 
  
 
