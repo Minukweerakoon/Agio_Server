88a8984c8e7e331fe047738860b14a4011a53cdd
Devangap <101203575+Devangap@users.noreply.github.com>
2024-05-05 19:57:41 +0530

    ghjg

--- schedulers/medEmailScheduler.js
+++ schedulers/medEmailScheduler.js
@@ -1,112 +1,112 @@
- const schedule = require('node-schedule');
- const nodemailer = require("nodemailer");
- require("dotenv").config();
+//  const schedule = require('node-schedule');
+//  const nodemailer = require("nodemailer");
+//  require("dotenv").config();
 
- const EventEmitter = require('events');
- const emitter = new EventEmitter();
+//  const EventEmitter = require('events');
+//  const emitter = new EventEmitter();
 
- // Run the medMonthlyReport
- const generateReport = require("../required/medMonthlyReport");
-
-
- // Get doctor's email
- var DOCTOR_EMAIL = null;
- const getDoctorEmail = async () => {
-     DOCTOR_EMAIL = "dinuravimukthi66@gmail.com";
- }
-
- getDoctorEmail();
+//  // Run the medMonthlyReport
+//  const generateReport = require("../required/medMonthlyReport");
 
 
+//  // Get doctor's email
+//  var DOCTOR_EMAIL = null;
+//  const getDoctorEmail = async () => {
+//      DOCTOR_EMAIL = "dinuravimukthi66@gmail.com";
+//  }
+
+//  getDoctorEmail();
 
 
- // Transporter
- const transporter = nodemailer.createTransport({
-     service: 'gmail',
-     host: "smtp.gmail.com",
-     port: 587,
-     secure: false, // Use `true` for port 465, `false` for all other ports
-     auth: {
-       user: process.env.EMAIL_USER, // Sender gmail address
-       pass: process.env.EMAIL_APP_PASSWORD, // App password from gmail account
-     },
-   });
+
+
+//  // Transporter
+//  const transporter = nodemailer.createTransport({
+//      service: 'gmail',
+//      host: "smtp.gmail.com",
+//      port: 587,
+//      secure: false, // Use `true` for port 465, `false` for all other ports
+//      auth: {
+//        user: process.env.EMAIL_USER, // Sender gmail address
+//        pass: process.env.EMAIL_APP_PASSWORD, // App password from gmail account
+//      },
+//    });
   
 
- // Options for the email
- var mailOptions = {};
+//  // Options for the email
+//  var mailOptions = {};
 
- const setMailOptions = (monthName) => {
+//  const setMailOptions = (monthName) => {
     
-     mailOptions = {
-         from: {
-             name: "Agio Support",
-             address: process.env.EMAIL_USER, 
-         }, 
-         to: [DOCTOR_EMAIL], 
-         subject: "Agio Medical Appointment Monthly Report", 
-         text: `This is the report for the month ${monthName}. Please check the attachments for the report document.`, 
-         html: `<p>This is the report for the month <b>${monthName}</b> <b></b>.<br>Please check the attachments for the report document.</p>`, // html body
-         attachments: [
-             {
-                 filename: 'Report.pdf',
-                 path: 'C://Users//Dinura Vimukthi//Documents//GitHub//agio-server//report.pdf',//'C://Users//Dinura Vimukthi//Documents//GitHub//agio-server//pdf//Report.pdf'
-                 contentType: 'application/pdf'
-             },
-         ]
-     };
- }
+//      mailOptions = {
+//          from: {
+//              name: "Agio Support",
+//              address: process.env.EMAIL_USER, 
+//          }, 
+//          to: [DOCTOR_EMAIL], 
+//          subject: "Agio Medical Appointment Monthly Report", 
+//          text: `This is the report for the month ${monthName}. Please check the attachments for the report document.`, 
+//          html: `<p>This is the report for the month <b>${monthName}</b> <b></b>.<br>Please check the attachments for the report document.</p>`, // html body
+//          attachments: [
+//              {
+//                  filename: 'Report.pdf',
+//                  path: 'C://Users//Dinura Vimukthi//Documents//GitHub//agio-server//report.pdf',//'C://Users//Dinura Vimukthi//Documents//GitHub//agio-server//pdf//Report.pdf'
+//                  contentType: 'application/pdf'
+//              },
+//          ]
+//      };
+//  }
 
 
- /*
- Send the email
- */
- const sendEmail = async (transporter, mailOptions) => {
-     try {
-         const response = await transporter.sendMail(mailOptions);
+//  /*
+//  Send the email
+//  */
+//  const sendEmail = async (transporter, mailOptions) => {
+//      try {
+//          const response = await transporter.sendMail(mailOptions);
 
-         // Log the response
-         console.log(`@sendEmail() @medEmailScheduler() Response => ${response.messageId}`)
-     } catch (error) {
-         console.log("Error occured when sending the email @sendEmail() @medEmailScheduler() => ", error);
-     }
- }
+//          // Log the response
+//          console.log(`@sendEmail() @medEmailScheduler() Response => ${response.messageId}`)
+//      } catch (error) {
+//          console.log("Error occured when sending the email @sendEmail() @medEmailScheduler() => ", error);
+//      }
+//  }
 
 
 
- /*
- emitter.on('medEmailScheduler', () => {
-     console.log('got medEmailScheduler event @medEmailScheduler()');
- }) */
+//  /*
+//  emitter.on('medEmailScheduler', () => {
+//      console.log('got medEmailScheduler event @medEmailScheduler()');
+//  }) */
 
-/*
- * 
- * 
- Scheduler for generating the email
- * 
- */
-// Run the scheduler at 00:10 on the 1st day of every month => '10 0 1 * *'
-// For testing => '* * * * * *'
-const medMonthlyReportGenerateScheduler = schedule.scheduleJob('10 0 1 * *', () => {
-    console.log('Med email scheduler ran');
+// /*
+//  * 
+//  * 
+//  Scheduler for generating the email
+//  * 
+//  */
+// // Run the scheduler at 00:10 on the 1st day of every month => '10 0 1 * *'
+// // For testing => '* * * * * *'
+// const medMonthlyReportGenerateScheduler = schedule.scheduleJob('10 0 1 * *', () => {
+//     console.log('Med email scheduler ran');
     
-    generateReport();
-    //medMonthlyReportGenerateScheduler.cancel();
-})
+//     generateReport();
+//     //medMonthlyReportGenerateScheduler.cancel();
+// })
 
-/*
- * 
- * 
- Scheduler for sending the mail
- * 
- */
-// Run the scheduler at 00:30 on the 1st day of every month => '30 0 1 * *' 
-// For testing => '* * * * * *'
-const medMonthlyReportScheduler = schedule.scheduleJob('30 0 1 * *', () => {
+// /*
+//  * 
+//  * 
+//  Scheduler for sending the mail
+//  * 
+//  */
+// // Run the scheduler at 00:30 on the 1st day of every month => '30 0 1 * *' 
+// // For testing => '* * * * * *'
+// const medMonthlyReportScheduler = schedule.scheduleJob('30 0 1 * *', () => {
 
-     var monthName = new Date(new Date().setDate(new Date().getDate()-1)).toLocaleString('default', { month: 'long' })
-     setMailOptions(monthName);
-     sendEmail(transporter, mailOptions);
+//      var monthName = new Date(new Date().setDate(new Date().getDate()-1)).toLocaleString('default', { month: 'long' })
+//      setMailOptions(monthName);
+//      sendEmail(transporter, mailOptions);
     
-    //medMonthlyReportScheduler.cancel();
-})
+//     //medMonthlyReportScheduler.cancel();
+// })
